7a512f3ff8f23cbed27759681cc43e4cff740628
Nyuminh <minhdlhse180733@fpt.edu.vn>
2026-05-12 17:05:04 +0700

    fix: implementation of db connection caching for vercel serverless

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose');
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) {
+    console.log('=> Using existing database connection');
+    return;
+  }
+
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000, // Timeout sau 5s thay vi 30s
+    });
+
+    isConnected = db.connections[0].readyState;
+    console.log(`✅ MongoDB Connected: ${db.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB Connection Error: ${error.message}`);
     // Khong su dung process.exit(1) tren Vercel
+    throw error; // Throw de Middleware handler co the bat duoc
   }
 };
 
